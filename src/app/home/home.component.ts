77b44e7320055a59d329edf7e6448d0318fdd73b
David Barcenas <dbarcenas.dev@gmail.com>
2023-11-20 21:24:14 -0600

    shop: add header styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NgForOf } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { LogoComponent } from '../core/components/logo/logo.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgForOf, RouterLink],
+  imports: [NgForOf, RouterLink, LogoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { LogoComponent } from '../core/components/logo/logo.component';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, LogoComponent],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
 })
