7134d0d50a7732a12e6c16309186cd2f8ee2ff3a
David Barcenas <dbarcenas.dev@gmail.com>
2023-11-27 14:57:36 -0600

    shop: get and show data

--- src/app/shop/products/product-detail/product-detail.component.ts
+++ src/app/shop/products/product-detail/product-detail.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, DestroyRef, inject, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Product } from '../product.interface';
+import { ProductService } from '../service/product.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-product-detail',
@@ -8,11 +10,25 @@ import { Product } from '../product.interface';
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss',
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnChanges {
   @Input() productId = 0;
+
+  private productService = inject(ProductService);
+  private destroyRef = inject(DestroyRef);
+
   product: Product | null = null;
   pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
   errorMessage = '';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const id = changes['productId'].currentValue;
+    if (id) {
+      this.productService
+        .getProduct(this.productId)
+        .pipe(takeUntilDestroyed(this.destroyRef))
+        .subscribe((product) => (this.product = product));
+    }
+  }
 
   addToCart(product: Product): void {}
 }
--- src/app/shop/products/product-list/product-list.component.ts
+++ src/app/shop/products/product-list/product-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../product.interface';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductService } from '../service/product.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-product-list',
@@ -11,11 +12,20 @@ import { ProductService } from '../service/product.service';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   private productService = inject(ProductService);
+  private destroyRef = inject(DestroyRef);
+
   products: Product[] = [];
   selectedProductId = 0;
   errorMessage = '';
+
+  ngOnInit(): void {
+    this.productService
+      .getProducts()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((products) => (this.products = products));
+  }
 
   onSelected(productId: number): void {
     this.selectedProductId = productId;
