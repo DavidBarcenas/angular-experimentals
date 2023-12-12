c1c7287e83e4c9c36280a5a5586e9bd88c6cdc94
David Barcenas <dbarcenas.dev@gmail.com>
2023-12-11 21:02:24 -0600

    tasks: render, add and remove tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.scss'
+  styleUrl: './tasks.component.scss',
 })
 export class TasksComponent {
+  tasks = signal(['Learn TypeScript 5.0']);
 
+  addTask(event: Event) {
+    const newTask = (event.target as HTMLInputElement).value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(idx: number) {
+    this.tasks.update((tasks) => tasks.filter((task, index) => idx !== index));
+  }
 }
