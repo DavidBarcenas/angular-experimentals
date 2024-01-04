e96fce8c8d9077cabd57fb79f358fc4226d69a12
David Barcenas <dbarcenas.dev@gmail.com>
2024-01-03 21:02:56 -0600

    shop: refactor product card

--- src/app/shop/products/product-card/product-card.component.ts
+++ src/app/shop/products/product-card/product-card.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Product } from '../product.interface';
 import { CartService } from '../../cart/service/cart.service';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
