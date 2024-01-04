4b2ba57a24f987c7200ac1c6620eb1f36ea6c05d
David Barcenas <dbarcenas.dev@gmail.com>
2024-01-03 21:35:45 -0600

    shop: add animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(), provideRouter(routes, withComponentInputBinding())],
+  providers: [
+    provideHttpClient(),
+    provideAnimations(),
+    provideRouter(routes, withComponentInputBinding()),
+  ],
 };
--- src/app/shop/products/product-card/product-card.component.ts
+++ src/app/shop/products/product-card/product-card.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Product } from '../product.interface';
 import { CartService } from '../../cart/service/cart.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-product-card',
@@ -10,14 +11,27 @@ import { CartService } from '../../cart/service/cart.service';
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [style({ opacity: 0 }), animate('350ms', style({ opacity: 1 }))]),
+      transition(':leave', [animate('100ms', style({ opacity: 0 }))]),
+    ]),
+  ],
 })
 export class ProductCardComponent {
   @Input({ required: true }) product!: Product;
+
+  trigger = true;
 
   private cartService = inject(CartService);
 
   addToCart(event: Event) {
     event.stopPropagation();
     this.cartService.addToCart(this.product);
+  }
+
+  open(event: Event) {
+    event.stopPropagation();
+    this.trigger = !this.trigger;
   }
 }
