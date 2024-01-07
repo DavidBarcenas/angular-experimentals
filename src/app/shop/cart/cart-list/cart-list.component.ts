57fe59dfcacb6ed4a28deb628a1169783e0fe676
David Barcenas <dbarcenas.dev@gmail.com>
2024-01-06 22:01:35 -0600

    shop: separate cart list

--- src/app/shop/cart/cart-list/cart-list.component.ts
+++ src/app/shop/cart/cart-list/cart-list.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { CartService } from '../service/cart.service';
+import { CartItem } from '../cart';
 
 @Component({
   selector: 'app-cart-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CurrencyPipe],
   templateUrl: './cart-list.component.html',
-  styleUrl: './cart-list.component.scss'
+  styleUrl: './cart-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CartListComponent {
+  private cartService = inject(CartService);
+  cartItems = this.cartService.cartItems;
 
+  addQuantity(cartItem: CartItem): void {
+    this.cartService.updateQuantity(cartItem, cartItem.quantity + 1);
+  }
+  removeFromCart(cartItem: CartItem): void {
+    this.cartService.removeFromCart(cartItem);
+  }
+
+  subtractQuantity(cartItem: CartItem): void {
+    if (cartItem.quantity > 1) {
+      this.cartService.updateQuantity(cartItem, cartItem.quantity - 1);
+    }
+  }
 }
