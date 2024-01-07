9abb5444e9c8e117fe435241b48077c0de621fd0
David Barcenas <dbarcenas.dev@gmail.com>
2024-01-06 22:01:27 -0600

    shop: separate cart summary

--- src/app/shop/cart/cart-summary/cart-summary.component.ts
+++ src/app/shop/cart/cart-summary/cart-summary.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { CartService } from '../service/cart.service';
 
 @Component({
   selector: 'app-cart-summary',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CurrencyPipe],
   templateUrl: './cart-summary.component.html',
-  styleUrl: './cart-summary.component.scss'
+  styleUrl: './cart-summary.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CartSummaryComponent {
-
+  private cartService = inject(CartService);
+  cartItems = this.cartService.cartItems;
+  subTotal = this.cartService.subTotal;
+  deliveryFee = this.cartService.deliveryFee;
+  tax = this.cartService.tax;
+  total = this.cartService.total;
 }
