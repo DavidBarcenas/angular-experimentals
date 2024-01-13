09ebf6dfbf55344ba6fbf8388058fb7a73248a1b
David Barcenas <dbarcenas.dev@gmail.com>
2024-01-13 13:54:53 -0600

    setup angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,5 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideAnimations(),
     provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
-  ],
+    provideAnimations()
+],
 };
