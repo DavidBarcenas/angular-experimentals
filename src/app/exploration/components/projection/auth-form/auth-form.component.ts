ee7d8bb197222a6adb0ff2c94527ea466511c011
David Barcenas <dbarcenas.dev@gmail.com>
2024-03-26 22:27:51 -0600

    access the content of the projected child

--- src/app/exploration/components/projection/auth-form/auth-form.component.ts
+++ src/app/exploration/components/projection/auth-form/auth-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, output } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { AfterContentInit, Component, ContentChild, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AuthRememberComponent } from '../auth-remember/auth-remember.component';
 
 interface User {
   email: string;
@@ -9,7 +11,7 @@ interface User {
 @Component({
   selector: 'app-auth-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   template: `
     <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
       <ng-content select="h3" />
@@ -22,13 +24,22 @@ interface User {
         <input type="password" name="password" ngModel />
       </label>
       <ng-content select="app-auth-remember" />
+      <p *ngIf="showMessage">You will be logged in for 30 days</p>
       <ng-content select="button" />
     </form>
   `,
   styleUrl: './auth-form.component.scss',
 })
-export class AuthFormComponent {
+export class AuthFormComponent implements AfterContentInit {
+  @ContentChild(AuthRememberComponent) remember: AuthRememberComponent | undefined;
   submitted = output<User>();
+  showMessage = false;
+
+  ngAfterContentInit() {
+    if (this.remember) {
+      this.remember.checked.subscribe((checked) => (this.showMessage = checked));
+    }
+  }
 
   onSubmit(value: User) {
     this.submitted.emit(value);
